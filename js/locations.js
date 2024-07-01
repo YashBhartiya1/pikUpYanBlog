1c9555e9f9d011033adb12cb3631f47946255d5e
yash <yashbhartiya03@gmail.com>
2024-07-01 17:24:35 +0530

    Initial commit

---
+++ js/locations.js
@@ -0,0 +1,346 @@
+let locationData = [
+  {
+    locationName: "dharamshala",
+    url: "http://127.0.0.1:5501/dharamshala.html",
+  },
+  {
+    locationName: "shimla",
+    url: "http://127.0.0.1:5501/shimla.html",
+  },
+  {
+    locationName: "stadium",
+    url: "http://127.0.0.1:5501/stadium.html",
+  },
+  {
+    locationName: "war memorial",
+    url: "http://127.0.0.1:5501/war.html",
+  },
+  {
+    locationName: "triund",
+    url: "http://127.0.0.1:5501/triund.html",
+  },
+  {
+    locationName: "bhagsu waterfall",
+    url: "http://127.0.0.1:5501/waterfall.html",
+  },
+  {
+    locationName: "tea garden",
+    url: "http://127.0.0.1:5501/tea.html",
+  },
+  {
+    locationName: "gyuto monastery",
+    url: "http://127.0.0.1:5501/gyuto.html",
+  },
+  {
+    locationName: "dharamkot",
+    url: "http://127.0.0.1:5501/Dharamkot.html",
+  },
+  {
+    locationName: "norbulingka",
+    url: "http://127.0.0.1:5501/Norbulingka.html",
+  },
+  {
+    locationName: "indrahar pass",
+    url: "http://127.0.0.1:5501/indrahar.html",
+  },
+  {
+    locationName: "namgyal",
+    url: "http://127.0.0.1:5501/namgyal.html",
+  },
+  {
+    locationName: "aghanjar mahadev",
+    url: "http://127.0.0.1:5501/aghanjar.html",
+  },
+  {
+    locationName: "tibetian library",
+    url: "http://127.0.0.1:5501/library.html",
+  },
+  {
+    locationName: "kareri lake",
+    url: "http://127.0.0.1:5501/kareri.html",
+  },
+  {
+    locationName: "church dharamshala",
+    url: "http://127.0.0.1:5501/church.html",
+  },
+  {
+    locationName: "bhagsunag",
+    url: "http://127.0.0.1:5501/bhagsunag.html",
+  },
+  {
+    locationName: "kangra art",
+    url: "http://127.0.0.1:5501/kangra art.html",
+  },
+  {
+    locationName: "tsuglagkhang Complex",
+    url: "http://127.0.0.1:5501/Tsuglagkhang.html",
+  },
+  {
+    locationName: "kalchakra",
+    url: "http://127.0.0.1:5501/kalchakra.html",
+  },
+  {
+    locationName: "lahesh caves",
+    url: "http://127.0.0.1:5501/lahesh.html",
+  },
+  {
+    locationName: "tapovan",
+    url: "http://127.0.0.1:5501/tapovan.html",
+  },
+  {
+    locationName: "ridge",
+    url: "http://127.0.0.1:5501/Ridge.html",
+  },
+  {
+    locationName: "kufri",
+    url: "http://127.0.0.1:5501/kufri.html",
+  },
+  {
+    locationName: "jakhu",
+    url: "http://127.0.0.1:5501/jakhu.html",
+  },
+  {
+    locationName: "museum shimla",
+    url: "http://127.0.0.1:5501/johnnie.html",
+  },
+  {
+    locationName: "sankat mochan",
+    url: "http://127.0.0.1:5501/sankatmochan.html",
+  },
+  {
+    locationName: "chadwick",
+    url: "http://127.0.0.1:5501/chadwick.html",
+  },
+  {
+    locationName: "kali bari temple",
+    url: "http://127.0.0.1:5501/kalibari.html",
+  },
+  {
+    locationName: "annadale",
+    url: "http://127.0.0.1:5501/annadale.html",
+  },
+  {
+    locationName: "tara devi",
+    url: "http://127.0.0.1:5501/tara.html",
+  },
+  {
+    locationName: "monastery shimla",
+    url: "http://127.0.0.1:5501/dorje.html",
+  },
+  {
+    locationName: "advance study",
+    url: "http://127.0.0.1:5501/viceregal.html",
+  },
+  {
+    locationName: "green valley",
+    url: "http://127.0.0.1:5501/green.html",
+  },
+  {
+    locationName: "mall road manali",
+    url: "http://127.0.0.1:5501/mall.html",
+  },
+  {
+    locationName: "vashisht temple",
+    url: "http://127.0.0.1:5501/vashisht.html",
+  },
+  {
+    locationName: "jogini",
+    url: "http://127.0.0.1:5501/jogini.html",
+  },
+  {
+    locationName: "solang valley",
+    url: "http://127.0.0.1:5501/solang.html",
+  },
+  {
+    locationName: "naggar",
+    url: "http://127.0.0.1:5501/naggar.html",
+  },
+  {
+    locationName: "rozy waterfall",
+    url: "http://127.0.0.1:5501/rozy.html",
+  },
+  {
+    locationName: "rohtang pass",
+    url: "http://127.0.0.1:5501/rohtang.html",
+  },
+  {
+    locationName: "hadimba",
+    url: "http://127.0.0.1:5501/hadimba.html",
+  },
+  {
+    locationName: "rahala waterfall",
+    url: "http://127.0.0.1:5501/rahala.html",
+  },
+  {
+    locationName: "manikaran",
+    url: "http://127.0.0.1:5501/manikaran.html",
+  },
+  {
+    locationName: "bhrigu lake",
+    url: "http://127.0.0.1:5501/bhrigu.html",
+  },
+  {
+    locationName: "manu temple",
+    url: "http://127.0.0.1:5501/manu.html",
+  },
+  {
+    locationName: "sagoo waterfall",
+    url: "http://127.0.0.1:5501/sagoo.html",
+  },
+  {
+    locationName: "himalaya nyingmapa",
+    url: "http://127.0.0.1:5501/nyingmapa.html",
+  },
+  {
+    locationName: "hampta pass",
+    url: "http://127.0.0.1:5501/hampta.html",
+  },
+  {
+    locationName: "wildlife sanctuary",
+    url: "http://127.0.0.1:5501/wildlife.html",
+  },
+  {
+    locationName: "arjun gufa",
+    url: "http://127.0.0.1:5501/arjun.html",
+  },
+  {
+    locationName: "knoi waterfall",
+    url: "http://127.0.0.1:5501/knoi.html",
+  },
+  {
+    locationName: "museum manali",
+    url: "http://127.0.0.1:5501/museum_HCFA.html",
+  },
+  {
+    locationName: "beas kund",
+    url: "http://127.0.0.1:5501/beas.html",
+  },
+  {
+    locationName: "bijli mahadev",
+    url: "http://127.0.0.1:5501/bijli.html",
+  },
+  {
+    locationName: "the great himalayan national park manali",
+    url: "http://127.0.0.1:5501/GHNP.html",
+  },
+  {
+    locationName: "anjani mahadev",
+    url: "http://127.0.0.1:5501/anjani.html",
+  },
+  {
+    locationName: "nehru kund",
+    url: "http://127.0.0.1:5501/nehru.html",
+  },
+  {
+    locationName: "prashar",
+    url: "http://127.0.0.1:5501/prashar.html",
+  },
+  {
+    locationName: "bhimakali",
+    url: "http://127.0.0.1:5501/bhimakali.html",
+  },
+  {
+    locationName: "barot",
+    url: "http://127.0.0.1:5501/barot.html",
+  },
+  {
+    locationName: "mandi gurudwara",
+    url: "http://127.0.0.1:5501/palangsahib.html",
+  },
+  {
+    locationName: "rewalsar",
+    url: "http://127.0.0.1:5501/rewalsar.html",
+  },
+  {
+    locationName: "shikari devi",
+    url: "http://127.0.0.1:5501/shikari.html",
+  },
+  {
+    locationName: "kamrunag",
+    url: "http://127.0.0.1:5501/kamrunag.html",
+  },
+  {
+    locationName: "kamlah fort",
+    url: "http://127.0.0.1:5501/kamlah.html",
+  },
+  {
+    locationName: "panchvaktra",
+    url: "http://127.0.0.1:5501/panchvaktra.html",
+  },
+  {
+    locationName: "pandoh dam",
+    url: "http://127.0.0.1:5501/pandoh.html",
+  },
+  {
+    locationName: "bhootnath",
+    url: "http://127.0.0.1:5501/bhootnath.html",
+  },
+  {
+    locationName: "karsog",
+    url: "http://127.0.0.1:5501/karsog.html",
+  },
+  {
+    locationName: "tarna devi",
+    url: "http://127.0.0.1:5501/tarna.html",
+  },
+  {
+    locationName: "triloknath",
+    url: "http://127.0.0.1:5501/triloknath.html",
+  },
+  {
+    locationName: "chamba chaugan",
+    url: "http://127.0.0.1:5501/chaugan.html",
+  },
+  {
+    locationName: "rang mahal",
+    url: "http://127.0.0.1:5501/rang.html",
+  },
+  {
+    locationName: "bhalei mata",
+    url: "http://127.0.0.1:5501/bhalei.html",
+  },
+  {
+    locationName: "bhuri singh museum",
+    url: "http://127.0.0.1:5501/bhuri.html",
+  },
+  {
+    locationName: "manimahesh",
+    url: "http://127.0.0.1:5501/manimahesh.html",
+  },
+  {
+    locationName: "lakshmi narayan temple",
+    url: "http://127.0.0.1:5501/LakshmiNarayan.html",
+  },
+  {
+    locationName: "akhand chandi palace",
+    url: "http://127.0.0.1:5501/akhandChandi.html",
+  },
+  {
+    locationName: "chamera lake",
+    url: "http://127.0.0.1:5501/chamera.html",
+  },
+  {
+    locationName: "church chamba",
+    url: "http://127.0.0.1:5501/scotland.html",
+  },
+  {
+    locationName: "chamunda devi",
+    url: "http://127.0.0.1:5501/chamunda chamba.html",
+  },
+  {
+    locationName: "sach pass",
+    url: "http://127.0.0.1:5501/sach.html",
+  },
+  {
+    locationName: "khajjiar lake",
+    url: "http://127.0.0.1:5501/khajjiar.html",
+  },
+  {
+    locationName: "kalatop wildlife sanctuary",
+    url: "http://127.0.0.1:5501/Kalatop Wildlife.html",
+  },
+  {
+    locationName: "dainkund peak",
+    url: "http://127.0.0.1:5501/dainkund.html",
+  },
+];
